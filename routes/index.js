41f96cf76acc8d651ce5ac273e2d470a8eab0772
C1323094 Daysa de Campos da Silva <daysacsilva@gmail.com>
2021-12-03 17:55:48 -0300

    fix POST '/search'

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Weather Forecast App' });
 });
+
+router.post('/search', (req, res, next) => {
+  const addressURI = encodeURI(req.body.location);
+  const cat = req.body.cat;
+
+  //const urlGeo = `https://api.mapbox.com/geocoding/v5/mapbox.places/${addressURI}.json?access_token=${apiKey}&limit=1`
+  res.send(req.body)
+  // request.get(movieUrl, (error, response, movieData) => {
+  //   const parsedData = JSON.parse(movieData);
+
+  //   if (cat =="person") {
+  //     parsedData.results = parsedData.results[0].known_for;
+  //   }
+    
+  //   res.render('index', {
+  //     parsedData: parsedData.results
+  //   })
+  // })
+})
 
 module.exports = router;
