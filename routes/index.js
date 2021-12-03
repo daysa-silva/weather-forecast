cc2afede6e0faf3d8868e4915a1d0ea1a3f844e1
C1323094 Daysa de Campos da Silva <daysacsilva@gmail.com>
2021-12-03 20:30:41 -0300

    geocoding api

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request')
+require('dotenv').config();
 
+const apiKey = process.env.API_KEY_GEO
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,19 +14,28 @@ router.post('/search', (req, res, next) => {
   const addressURI = encodeURI(req.body.location);
   const cat = req.body.cat;
 
-  //const urlGeo = `https://api.mapbox.com/geocoding/v5/mapbox.places/${addressURI}.json?access_token=${apiKey}&limit=1`
-  res.send(req.body)
-  // request.get(movieUrl, (error, response, movieData) => {
-  //   const parsedData = JSON.parse(movieData);
+  const urlGeo = `https://api.mapbox.com/geocoding/v5/mapbox.places/${addressURI}.json?access_token=${apiKey}&limit=1`
+  
+  request({url: urlGeo, json: true}, (error, response, callback) => {
+    if (error) {
+      res.locals.message = 'Unable to connect to gecoding service!';
+      res.locals.error = error
+      res.render('error')
+    } else if(response.body.message || response.body.features.length === 0) {
+      res.locals.message = 'Unable to find a place! Try another search.';
+      res.locals.error = response.body.message || ''
+      res.render('error')
+    } else {
+        const dataLocation = response.body.features[0];
 
-  //   if (cat =="person") {
-  //     parsedData.results = parsedData.results[0].known_for;
-  //   }
+        res.send( {
+            location: dataLocation.place_name,
+            latitude: dataLocation.center[1],
+            longitude: dataLocation.center[0]
+        })
+    }
     
-  //   res.render('index', {
-  //     parsedData: parsedData.results
-  //   })
-  // })
+  })
 })
 
 module.exports = router;
